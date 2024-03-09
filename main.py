cd647ccc42719e5a750a2338d837110c6ddd086d
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-03-09 09:53:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ import os
 
 bot = Client(
     "bot",
-    bot_token=os.environ.get("6950666016:AAHC_AuTsdLyXfKHlEiLtsKyc0Obdc4kOKU"),
-    api_id=int(os.environ.get("29403305")),
-    api_hash=os.environ.get("54c435573d0f2d9dada270a0cb493b36")
+    bot_token=os.environ.get("7141439180:AAGDZM4wIbOf6kcXOgw1GR5__xiMD2NFLlk"),
+    api_id=int(os.environ.get("25919081")),
+    api_hash=os.environ.get("0bc2fdba14b16b44f0d89729ed8d2118")
 )
 auth_users = [ int(chat) for chat in os.environ.get("6381511858").split(",") if chat != '']
 sudo_users = auth_users
