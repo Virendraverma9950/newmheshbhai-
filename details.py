9f9e24a1f0f59da8118f1c68f7fedcf89ba54419
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-03-09 10:01:28 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 29403305
-api_hash = "54c435573d0f2d9dada270a0cb493b36"
-bot_token = "6950666016:AAHC_AuTsdLyXfKHlEiLtsKyc0Obdc4kOKU"
-sudo_groups =-1002013062742
+api_id = 25919081
+api_hash = "0bc2fdba14b16b44f0d89729ed8d2118"
+bot_token = "7141439180:AAGDZM4wIbOf6kcXOgw1GR5__xiMD2NFLlk"
+sudo_groups =-1002015954185
 
